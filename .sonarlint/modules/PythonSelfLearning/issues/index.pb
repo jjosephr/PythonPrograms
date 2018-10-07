8270d26ceb9e138ac4b35fb8750b4243cce0bd3b
jjosephr <josphselvin@yahoo.com>
2018-10-07 13:40:43 +0530

    Python Programs on Eclipse IDE...

---
+++ .sonarlint/modules/PythonSelfLearning/issues/index.pb
@@ -0,0 +1,3 @@
+
+C
+src/FirstProgram.py,7\f\7f6d2cbf94dbf50f1c9559f1d660615bbfaa9da5
---
+++ PythonSelfLearning/src/FirstProgram.py
@@ -0,0 +1,6 @@
+'''
+Created on Oct 7, 2018
+
+@author: JJOSEPHR
+'''
+print("Welcome to the world of Python...!!!")

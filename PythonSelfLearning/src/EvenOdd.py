4c9b440f8546c1f1b8f4a9dc41e845dfaa4972e0
jjosephr <josphselvin@yahoo.com>
2018-10-08 18:50:13 +0530

    Added EvenOdd.py and modified FirstProgram.py

---
+++ PythonSelfLearning/src/EvenOdd.py
@@ -0,0 +1,12 @@
+'''
+Write a shell script to find whether an input integer is even or odd
+
+Created on Sep 8, 2018
+
+@author: JJOSEPHR
+'''
+v = int(input("Enter a value: "))
+if (v%2 ) == 0:
+    print("Even number")
+else:
+    print("Odd number")
--- PythonSelfLearning/src/FirstProgram.py
+++ PythonSelfLearning/src/FirstProgram.py
@@ -3,4 +3,4 @@ Created on Oct 7, 2018
 
 @author: JJOSEPHR
 '''
-print("Welcome to the world of Python...!!!")
+print("Welcome to the world of Python...!!")
